420b616f309c9850a765560001212c53041bc3fe
lisahoong <lhoong94@gmail.com>
2017-07-30 00:33:52 -0700

    updated server

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-import apiai from 'apiai';
-import express from 'express';
+const apiai = require('apiai');
+const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) {
+app.get('/', (req, res) => {
     res.send('ok done')
 })
 
 const port = process.env.PORT || 3000;
-app.listen(port,
-    (port) => {console.log('App started! Listening on port: ', port)}
+app.listen(port, () => {console.log('App started! Listening on port: ', port)}
 );
