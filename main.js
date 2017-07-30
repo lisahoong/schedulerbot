4455d11ac8122b447313cb78496d41f06195b9c3
lisahoong <lhoong94@gmail.com>
2017-07-30 00:59:36 -0700

    challenge

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const express = require('express');
 
 const app = express();
 
+app.post('/:challenge', (req, res) => {
+    console.log("param", req.params.challenge);
+    res.send(req.params.challenge);
+})
+
 app.get('/', (req, res) => {
     res.send('ok done')
 })
