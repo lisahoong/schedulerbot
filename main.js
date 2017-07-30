1912762dd9fa6a855ed2ab6286aaf095aeb8d9ff
lisahoong <lhoong94@gmail.com>
2017-07-30 00:30:00 -0700

    added start script

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ import apiai from 'apiai';
 import express from 'express';
 
 const app = express();
+
+app.get('/', (req, res) {
+    res.send('ok done')
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port,
+    (port) => {console.log('App started! Listening on port: ', port)}
+);
